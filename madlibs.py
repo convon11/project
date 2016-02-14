f5a75649ca063a8f0e43568f886b182fb04fb772
convon11 <corey@nordloh.com>
2016-02-15 03:35:51 +0500

    finished

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,12 @@
-f=()
-u=(
+#!/usr/bin/python3
+#",s,"
+verb = input('a verb:')
+u=input("a noun:")
+s=input("a adjective:")
+e=input("a plurle noun:")
+lib=("oh no, where is my",u,"which I use to ",verb," the ",s," ",e)
+
+print(u)
+print(s)
+print(e)
+print(lib)
